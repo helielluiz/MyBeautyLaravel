0add58923f7d488e3fd5b759df040751b2e40f46
mysoft-heliel <heliel@mysoftsistemas.com.br>
2018-11-20 09:54:08 -0200

    ajustes de rotas

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,14 @@ Route::post('Servico/{Servico}', 'ServicoController@store');
 Route::apiResource('Cidade', 'CidadeController');
 Route::post('Cidade/{Cidade}', 'CidadeController@store');
 
+Route::apiResource('Estabelecimento', 'EstabelecimentoController');
+Route::post('Estabelecimento/{Estabelecimento}', 'EstabelecimentoController@store');
+
+Route::apiResource('ServicoDoEstabelecimento', 'ServicoDoEstabelecimentoController');
+Route::post('ServicoDoEstabelecimento/{ServicoDoEstabelecimento}', 'ServicoDoEstabelecimentoController@store');
+
 Route::apiResource('Estado', 'EstadoController');
 Route::post('Estado/{Estado}', 'EstadoController@store');
-Route::put('Estado/{Estado}', 'EstadoController@update');
+Route::put('Estado/update', 'EstadoController@update');
 Route::get('Estado/{Estado}', 'EstadoController@show');
 
